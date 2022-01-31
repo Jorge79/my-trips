30312e1ef935df51b7d0a9555549b9a2ef2ab460
Jorge Francisco <Jorge7.9@hotmail.com>
2022-01-31 16:45:48 -0300

    feat: LinkWrapper component & tests

---
+++ src/components/LinkWrapper/test.tsx
@@ -0,0 +1,16 @@
+import { screen, render } from '@testing-library/react'
+import LinkWrapper from '.'
+
+describe('<LinkWraper />', () => {
+  it('should render link and children', () => {
+    render(<LinkWrapper href="/my-link">Anything</LinkWrapper>)
+    const children = screen.getByRole('link', {
+      name: /anything/i
+    })
+
+    expect(children).toBeInTheDocument()
+    expect(children).toHaveAttribute('href', '/my-link')
+
+    screen.logTestingPlaygroundURL()
+  })
+})
