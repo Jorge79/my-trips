6b857bc82846faa4ab99a525b5c723dc7c6f7b24
Jorge Francisco <Jorge7.9@hotmail.com>
2022-02-02 03:14:05 -0300

    feat: Create N generic static pages

---
+++ src/templates/Pages/index.tsx
@@ -0,0 +1,23 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import LinkWrapper from 'components/LinkWrapper'
+import * as S from './styles'
+
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>{heading}</S.Heading>
+    <S.Body>
+      <div dangerouslySetInnerHTML={{ __html: body }}></div>
+    </S.Body>
+  </S.Content>
+)
+
+export default PageTemplate
