5d6c9872258e1eaa0fa44f65de18a330ae5a7f19
Jorge Francisco <Jorge7.9@hotmail.com>
2022-02-01 22:18:39 -0300

    chore: query getPages, correction endpoint's var

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint = process.env.GRAPHQLHOST || ''
+const endpoint = process.env.GRAPHQL_HOST || ''
 
 const client = new GraphQLClient(endpoint, {
   headers: {
