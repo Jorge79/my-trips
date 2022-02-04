5b4f9f807b47ddbc442930251c53bb4945f31051
Jorge Francisco <Jorge7.9@hotmail.com>
2022-02-04 00:20:40 -0300

    feat: Styling place's pages

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,3 +1,7 @@
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import * as S from './styles'
+
 type ImageProps = {
   url: string
   height: number
@@ -18,11 +22,25 @@ export type PlacesTemplateProps = {
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
   return (
     <>
-      <h1>{place.name}</h1>
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-      {place.gallery.map((image, index) => (
-        <img key={`photo-${index}`} src={image.url} alt={place.name} />
-      ))}
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description.html }}
+          />
+
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <img key={`photo-${index}`} src={image.url} alt={place.name} />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
